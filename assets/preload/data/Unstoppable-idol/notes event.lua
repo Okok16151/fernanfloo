efd6c7d14552689ab65319e9c4d28fb354b15832
Okok16151 <97395275+Okok16151@users.noreply.github.com>
2022-02-26 20:32:14 +0800

    Add files via upload

---
+++ assets/preload/data/Unstoppable-idol/notes event.lua
@@ -0,0 +1,26 @@
+function onStepHit()
+    if curStep == 767 then
+        noteTweenAlpha("Note2", 2, 0, 1, "linear")
+    end
+    if curStep == 799 then
+        noteTweenAlpha("Note0", 0, 0, 1, "linear")
+    end
+    if curStep == 816 then
+	    noteTweenAlpha("Note1", 1, 0, 1, "linear")
+    end
+    if curStep == 824 then
+        noteTweenAlpha("Note3", 3, 0, 1, "linear")
+    end
+    if curStep == 831 then
+        noteTweenAlpha("Note6", 6, 0, 1, "linear")
+    end
+    if curStep == 864 then
+        noteTweenAlpha("Note4", 4, 0, 1, "linear")
+    end
+    if curStep == 888 then
+        noteTweenAlpha("Note7", 7, 0, 1, "linear")
+    end
+     if curStep == 880 then
+        noteTweenAlpha("Note5", 5, 0, 1, "linear")
+    end
+end
